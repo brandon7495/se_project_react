fe5b62b2d0dc637bc6013b2cfb6afcaebe0c6673
Brandon Holifield <brandonholifield31@gmail.com>
2025-07-13 04:32:28 -0700

    Styling & filtering cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
   return (
     <div className="app">
       <div className="app__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
         <Footer />
       </div>
     </div>
